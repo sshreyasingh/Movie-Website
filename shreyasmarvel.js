068a7993be56ac1311a3df4b9b90f9ad465d9b6c
sshreyasingh <161379376+sshreyasingh@users.noreply.github.com>
2024-07-18 01:09:45 +0530

    Add files via upload

---
+++ shreyasmarvel.js
@@ -0,0 +1,107 @@
+
+let loki=document.querySelector(".box1");
+loki.addEventListener("dblclick",()=>{
+    let link1=document.createElement('a');
+link1.href="https://www.youtube.com/watch?v=nW948Va-l10";
+    loki.appendChild(link1);
+    link1.click();
+console.log("clicked");
+});
+
+
+let wv=document.querySelector(".box2");
+wv.addEventListener("dblclick",()=>{
+    let link2=document.createElement('a');
+    link2.href="https://www.youtube.com/watch?v=sj9J2ecsSpo";
+    wv.appendChild(link2);
+    link2.click();
+});
+
+
+let mm=document.querySelector(".box3");
+mm.addEventListener("dblclick",()=>{
+    let link3=document.createElement('a');
+    link3.href="https://www.youtube.com/watch?v=m9EX0f6V11Y";
+    mm.appendChild(link3);
+    link3.click();
+});
+
+let hawk=document.querySelector(".box4");
+hawk.addEventListener("dblclick",()=>{
+    let link4=document.createElement('a');
+    link4.href="https://www.youtube.com/watch?v=5VYb3B1ETlk";
+    hawk.appendChild(link4);
+    link4.click();
+});
+
+let avengers=document.querySelector(".box12");
+avengers.addEventListener("dblclick",()=>{
+    let link5=document.createElement('a');
+    link5.href="https://www.youtube.com/watch?v=eOrNdBpGMv8";
+    avengers.appendChild(link5);
+    link5.click();
+});
+
+let aou=document.querySelector(".box22");
+aou.addEventListener("dblclick",()=>{
+    let link6=document.createElement('a');
+    link6.href="https://www.youtube.com/watch?v=tmeOjFno6Do";
+    aou.appendChild(link6);
+    link6.click();
+});
+
+let end=document.querySelector(".box32");
+end.addEventListener("dblclick",()=>{
+    let link7=document.createElement('a');
+    link7.href="https://www.youtube.com/watch?v=TcMBFSGVi1c";
+    end.appendChild(link7);
+    link7.click();
+});
+
+let iw=document.querySelector(".box42");
+iw.addEventListener("dblclick",()=>{
+    let link8=document.createElement('a');
+    link8.href="https://www.youtube.com/watch?v=6ZfuNTqbHE8";
+    iw.appendChild(link8);
+    link8.click();
+});
+
+let thor=document.querySelector(".box13");
+thor.addEventListener("dblclick",()=>{
+    let link9=document.createElement('a');
+    link9.href="https://www.youtube.com/watch?v=JOddp-nlNvQ";
+    thor.appendChild(link9);
+    link9.click();
+});
+
+let im3=document.querySelector(".box23");
+im3.addEventListener("dblclick",()=>{
+    let link10=document.createElement('a');
+    link10.href="https://www.youtube.com/watch?v=YLorLVa95Xo";
+    im3.appendChild(link10);
+    link10.click();
+});
+
+let cw=document.querySelector(".box33");
+cw.addEventListener("dblclick",()=>{
+    let link11=document.createElement('a');
+    link11.href="https://www.youtube.com/watch?v=dKrVegVI0Us";
+    cw.appendChild(link11);
+    link11.click();
+});
+
+let ds=document.querySelector(".box43");
+ds.addEventListener("dblclick",()=>{
+    let link12=document.createElement('a');
+    link12.href="https://www.youtube.com/watch?v=Rt_UqUm38BI";
+    ds.appendChild(link12);
+    link12.click();
+});
+
+let dw=document.querySelector(".box14");
+dw.addEventListener("dblclick",()=>{
+    let link13=document.createElement('a');
+    link13.href="https://www.youtube.com/watch?v=73_1biulkYk";
+    dw.appendChild(link13);
+    link13.click();
+});
